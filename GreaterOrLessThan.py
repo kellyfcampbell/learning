17cdcf46c2f0a52c27c7370e605c2ef4a8acc7a9
Campbell <kelly.campbell@gainwelltechnologies.com>
2022-06-09 20:40:04 -0400

    My first Python script

---
+++ GreaterOrLessThan.py
@@ -0,0 +1,12 @@
+first = input ("enter a number")
+second = input ("second number")
+z=first>second
+##print (z)
+if z is True:
+    print ("Larger number")
+    print (first)
+else:
+    print ("Larger number")
+    print (second)
+print ("Goodbye")
+
